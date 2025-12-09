627cdfbb084be3971bcb8b20b3614bc6b3f8aae5
Ahmed Tamer <ahmedibntamer@gmail.com>
2025-12-09 04:16:14 +0200

    UI prof with gpt

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,9 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:hungry/core/constants/app_colors.dart';
+import 'package:hungry/root.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView>
+    with SingleTickerProviderStateMixin {
+
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+  late Animation<Offset> _slideAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+
+    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeIn),
+    );
+
+
+    _scaleAnimation = Tween<double>(begin: 0.7, end: 1).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOutBack),
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(0, 0.3),
+      end: Offset.zero,
+    ).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+
+    _controller.forward();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (c) {
+        return Root();
+      }));
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +67,30 @@ class SplashView extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Gap(280),
-          Center(child: SvgPicture.asset('assets/logo/logo.svg')),
-          Spacer(),
-          Center(child: Image.asset('assets/splash/splash.png')),
+          const Gap(220),
+
+          Center(
+            child: FadeTransition(
+              opacity: _fadeAnimation,
+              child: ScaleTransition(
+                scale: _scaleAnimation,
+                child: SvgPicture.asset(
+                  'assets/logo/logo.svg',
+                ),
+              ),
+            ),
+          ),
+
+          const Spacer(),
+
+          SlideTransition(
+            position: _slideAnimation,
+            child: Center(
+              child: Image.asset(
+                'assets/splash/splash.png',
+              ),
+            ),
+          ),
         ],
       ),
     );
